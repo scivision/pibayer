8d28e03bc2205eef71192779b7409ebf0ca03431
Michael Hirsch <scivision@noreply.github.com>
2015-11-27 05:46:28 +0000

    query parameters

--- getrawimage.py
+++ getrawimage.py
@@ -18,6 +18,8 @@ import picamera.array as camarray
 def pibayerraw(fn,demosaic,quiet):
     tic = time()
     with PiCamera() as cam: #load camera driver
+        setparams(cam)
+        getparams(cam)
         with camarray.PiBayerArray(cam) as S: #prepare to read raw
             if not quiet:
                 print('{:.1f} sec to load API'.format(time()-tic));tic=time()
@@ -31,6 +33,7 @@ def pibayerraw(fn,demosaic,quiet):
                     print('{:.1f} sec to demosaic.'.format(time()-tic)); tic=time()
             else:
                 img10 = S.array
+    print(img10.shape)
 #%% linear scale 10-bit to 8-bit 
     img8 = bytescale(img10,0,1024,255,0)
     if not quiet: 
@@ -43,6 +46,31 @@ def pibayerraw(fn,demosaic,quiet):
 
     return img10,img8    
             
+def setparams(c):
+    c.awb_mode ='off' #auto white balance
+#   c.brightness(50)
+#   c.contrast(0)
+    c.drc_strength = 'off'
+    c.image_denoise = False
+    c.image_effect = 'none'
+
+def getparams(c):
+    print('analog / digital gain {} / {}'.format(c.analog_gain,c.digital_gain))
+    print('auto white balance {}'.format(c.awb_mode))
+    print('brightness {}'.format(c.brightness))
+    print('contrast {}'.format(c.contrast))    
+    print('dynamic range compression {}'.format(c.drc_strength))
+    print('exposure compensation {}'.format(c.exposure_compensation))
+    print('exposure mode {}'.format(c.exposure_mode))
+    print('exposure / shutter speed {} / {}'.format(c.exposure_speed,c.shutter_speed))
+    print('image denoise {}'.format(c.image_denoise))    
+    print('image effect {}'.format(c.image_effect))
+    print('ISO {}'.format(c.iso))
+    print('exposure metering mode {}'.format(c.meter_mode))
+    print('rotation angle {}'.format(c.rotation))
+    print('saturation {}'.format(c.saturation))
+    print('sharpness {}'.format(c.sharpness))
+
 if __name__ == '__main__':
     from argparse import ArgumentParser
     p = ArgumentParser(description='Raspberry Pi Picamera demo with raw Bayer data')
