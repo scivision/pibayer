f7cd72f8724389f7f672c474266f466e518a9d30
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-11-05 19:49:22 -0500

    assert instead of print

--- pibayer/__init__.py
+++ pibayer/__init__.py
@@ -209,7 +209,8 @@ def getparams(c:PiCamera):
     print('analog gain', float(c.analog_gain))
 
      #print('digital gain',float(c.digital_gain))
-    assert np.testing.assert_almost_equal(c.digital_gain, 1.)
+    assert np.testing.assert_almost_equal(c.digital_gain, 1.,
+                                          rtol=0.1)
 
     #print('auto white balance:',c.awb_mode)
     assert c.awb_mode=='off'
